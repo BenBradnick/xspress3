0a2e4f5c8767f366171775edb892682546651455
Ben Bradnick <ben@quantumdetectors.com>
2023-12-20 11:41:49 +0000

    Fix record macro for second channel mask plugin

--- iocs/xspress3IOC/iocBoot/common/DefineChannelSum.cmd
+++ iocs/xspress3IOC/iocBoot/common/DefineChannelSum.cmd
@@ -27,7 +27,7 @@ dbLoadRecords("$(ADCORE)/db/NDStdArrays.template", "P=$(PREFIX),R=MCA$(CHAN):,PO
 
 # Channel mask plugin to mask individual channels (set them to zero)
 XspressChannelMaskConfigure("$(PORT)SUM.CHANMASK", "$(NUM_CHANNELS)", "$(QSIZE)", 0, "PROC1", 0, 0, 0, 0, 0, 1)
-dbLoadRecords("xspress3ChannelMask.template", "P=$(PREFIX), R=det1:MASKSUM:, PORT=$(PORT).CHANMASK, NDARRAY_PORT=$(PORT)")
+dbLoadRecords("xspress3ChannelMask.template", "P=$(PREFIX), R=det1:MASKSUM:, PORT=$(PORT)SUM.CHANMASK, NDARRAY_PORT=$(PORT)")
 
 # ROI plugin to sum across all (non-zero) channels
 NDROIConfigure("CHANSUM$(CHAN)", "$(QSIZE)", 0, "$(PORT)SUM.CHANMASK", 0, -1, -1)
