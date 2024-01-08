2d968a931e09f084522770b6ef25fa02be76a5f3
Ben Bradnick <ben@quantumdetectors.com>
2024-01-08 11:38:16 +0000

    Initialise NDArray ports for mask plugins

--- iocs/xspress3IOC/iocBoot/common/DefineChannelSum.cmd
+++ iocs/xspress3IOC/iocBoot/common/DefineChannelSum.cmd
@@ -13,11 +13,11 @@ epicsEnvSet("CHM1",   "ALL")
 # Channel mask plugin to mask individual channels (set them to zero)
 XspressChannelMaskConfigure("$(PORT).CHANMASK", "$(NUM_CHANNELS)", "$(QSIZE)", 0, "XSP3", 0, 0, 0, 0, 0, 1)
 dbLoadRecords("xspress3ChannelMask.template", "P=$(PREFIX), R=det1:MASK:, PORT=$(PORT).CHANMASK, NDARRAY_PORT=$(PORT)")
-dbLoadRecords("xspress3ChannelMask_$(NUM_CHANNELS)Channel.template", "P=$(PREFIX), R=det1:MASK:, PORT=$(PORT).CHANMASK, NDARRAY_PORT=$(PORT)")
+dbLoadRecords("xspress3ChannelMask_$(NUM_CHANNELS)Channel.template", "P=$(PREFIX), R=det1:MASK:, PORT=$(PORT).CHANMASK")
 
 # ROI plugin to sum across all (non-zero) channels
 NDROIConfigure("CHAN$(CHAN)", "$(QSIZE)", 0, "$(PORT).CHANMASK", 0, -1, -1)
-dbLoadRecords("NDROI.template", "P=$(PREFIX), R=ROI$(CHAN):, PORT=CHAN$(CHAN), TIMEOUT=1, ADDR=0, NDARRAY_PORT=$(PORT), NDARRAY_ADDR=0, Enabled=0")
+dbLoadRecords("NDROI.template", "P=$(PREFIX), R=ROI$(CHAN):, PORT=CHAN$(CHAN), TIMEOUT=1, ADDR=0, NDARRAY_PORT=$(PORT).CHANMASK, NDARRAY_ADDR=0, Enabled=0")
 
 # Standard arrays for visualisation
 NDStdArraysConfigure("MCA$(CHAN)", 5, 0, "CHAN$(CHAN)", 0, 0)
@@ -28,12 +28,12 @@ dbLoadRecords("$(ADCORE)/db/NDStdArrays.template", "P=$(PREFIX),R=MCA$(CHAN):,PO
 
 # Channel mask plugin to mask individual channels (set them to zero)
 XspressChannelMaskConfigure("$(PORT)SUM.CHANMASK", "$(NUM_CHANNELS)", "$(QSIZE)", 0, "PROC1", 0, 0, 0, 0, 0, 1)
-dbLoadRecords("xspress3ChannelMask.template", "P=$(PREFIX), R=det1:MASKSUM:, PORT=$(PORT)SUM.CHANMASK, NDARRAY_PORT=$(PORT)")
-dbLoadRecords("xspress3ChannelMask_$(NUM_CHANNELS)Channel.template", "P=$(PREFIX), R=det1:MASKSUM:, PORT=$(PORT).CHANMASK, NDARRAY_PORT=$(PORT)")
+dbLoadRecords("xspress3ChannelMask.template", "P=$(PREFIX), R=det1:MASKSUM:, PORT=$(PORT)SUM.CHANMASK, NDARRAY_PORT=PROC1")
+dbLoadRecords("xspress3ChannelMask_$(NUM_CHANNELS)Channel.template", "P=$(PREFIX), R=det1:MASKSUM:, PORT=$(PORT)SUM.CHANMASK")
 
 # ROI plugin to sum across all (non-zero) channels
 NDROIConfigure("CHANSUM$(CHAN)", "$(QSIZE)", 0, "$(PORT)SUM.CHANMASK", 0, -1, -1)
-dbLoadRecords("NDROI.template", "P=$(PREFIX), R=ROISUM$(CHAN):, PORT=CHANSUM$(CHAN), TIMEOUT=1, ADDR=0, NDARRAY_PORT=PROC1, NDARRAY_ADDR=0, Enabled=0")
+dbLoadRecords("NDROI.template", "P=$(PREFIX), R=ROISUM$(CHAN):, PORT=CHANSUM$(CHAN), TIMEOUT=1, ADDR=0, NDARRAY_PORT=$(PORT)SUM.CHANMASK, NDARRAY_ADDR=0, Enabled=0")
 
 # Standard arrays for visualisation
 NDStdArraysConfigure("MCASUM$(CHAN)", 5, 0, "CHANSUM$(CHAN)", 0, 0)
